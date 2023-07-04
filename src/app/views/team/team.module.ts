53c92d039f685a0be9e6fb448c6d1c737692e82d
Henrique Fickert <henriquefickert@unip.br>
2023-07-04 18:46:44 -0300

    adição dos components de membro

--- src/app/views/team/team.module.ts
+++ src/app/views/team/team.module.ts
@@ -6,13 +6,17 @@ import { TeamComponent } from './team.component';
 import { TeamBannerComponent } from './team-banner/team-banner.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { FumetsuContentComponent } from './fumetsu-content/fumetsu-content.component';
+import { MembersComponent } from './members/members.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
 
 
 @NgModule({
   declarations: [
     TeamComponent,
     TeamBannerComponent,
-    FumetsuContentComponent
+    FumetsuContentComponent,
+    MembersComponent,
+    MemberCardComponent
   ],
   imports: [
     CommonModule,
