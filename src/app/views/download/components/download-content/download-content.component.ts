8596693a4fcddcba57231203592f3662115fb3ad
Henrique Fickert <henriquefickert@unip.br>
2023-07-11 15:27:38 -0300

    Troca do download para google drive

--- src/app/views/download/components/download-content/download-content.component.ts
+++ src/app/views/download/components/download-content/download-content.component.ts
@@ -9,17 +9,4 @@ export class DownloadContentComponent {
   public title: string = "Download";
   public white: boolean = true;
   public tag: string = "See what I see, feel what I feel";
-
-
-  downloadFile() {
-    const fileUrl = './assets/files/game/Disorder.rar';
-
-    const link = document.createElement('a');
-    link.href = fileUrl;
-    link.download = 'Disorder.rar';
-
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  }
 }
