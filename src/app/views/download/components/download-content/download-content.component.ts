8ee7597e6c36319a98c815bf07055deaf6e86c11
Henrique Fickert <henriquefickert@unip.br>
2023-07-11 14:13:59 -0300

    correção para download na build

--- src/app/views/download/components/download-content/download-content.component.ts
+++ src/app/views/download/components/download-content/download-content.component.ts
@@ -1,21 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-download-content',
   templateUrl: './download-content.component.html',
   styleUrls: ['./download-content.component.scss']
 })
-export class DownloadContentComponent {
+export class DownloadContentComponent implements OnInit {
   public title: string = "Download";
   public white: boolean = true;
   public tag: string = "See what I see, feel what I feel";
 
+  fileUrl!: SafeUrl;
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  ngOnInit() {
+    const filePath = './assets/files/game/Disorder.rar';
+    this.fileUrl = this.sanitizer.bypassSecurityTrustUrl(filePath);
+  }
 
   downloadFile() {
-    const fileUrl = './assets/files/game/Disorder.rar';
-
     const link = document.createElement('a');
-    link.href = fileUrl;
+    link.href = this.fileUrl.toString();
     link.download = 'Disorder.rar';
 
     document.body.appendChild(link);
