e513f3decfd271b481485c099f137d930642ef22
Henrique Fickert <henriquefickert@unip.br>
2023-07-11 14:44:41 -0300

    Teste

--- src/app/views/download/components/download-content/download-content.component.ts
+++ src/app/views/download/components/download-content/download-content.component.ts
@@ -1,29 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-download-content',
   templateUrl: './download-content.component.html',
   styleUrls: ['./download-content.component.scss']
 })
-export class DownloadContentComponent implements OnInit {
+export class DownloadContentComponent {
   public title: string = "Download";
   public white: boolean = true;
   public tag: string = "See what I see, feel what I feel";
 
-  fileUrl!: SafeUrl;
-
-  constructor(private sanitizer: DomSanitizer) { }
-
-  ngOnInit() {
-    const filePath = './assets/files/game/Disorder.rar';
-    this.fileUrl = this.sanitizer.bypassSecurityTrustUrl(filePath);
-  }
 
   downloadFile() {
+    const fileUrl = './assets/files/game/Disorder.rar';
+
     const link = document.createElement('a');
-    link.href = this.fileUrl.toString();
+    link.href = fileUrl;
     link.download = 'Disorder.rar';
 
     document.body.appendChild(link);
